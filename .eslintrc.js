76c33732ece292cc7ebc3a03b2adbc5a1920f1c9
hansol, park <winetree94@outlook.com>
2022-11-12 16:27:54 +0000

    chore: lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'react-app',
+    'react-app/jest',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-trailing-spaces': ['error'],
+    indent: ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'prettier/prettier': ['error'],
+  },
+};
