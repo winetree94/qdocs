3acb68fccaaa4f428053801fb53519192e9b5b6b
winetree94 <winetree94@outlook.com>
2023-01-24 01:20:55 +0900

    fix: Square margin not visible

--- src/components/queue/rect/Square.tsx
+++ src/components/queue/rect/Square.tsx
@@ -1,5 +1,34 @@
-import { FunctionComponent } from 'react';
+import clsx from 'clsx';
+import { FunctionComponent, useContext } from 'react';
+import { QueueObjectContainerContext } from '../Container';
+import { QueueAnimatableContext } from '../QueueAnimation';
 
 export const Square: FunctionComponent = () => {
-  return <div>square</div>;
+  const containerContext = useContext(QueueObjectContainerContext);
+  const animation = useContext(QueueAnimatableContext);
+  const margin = containerContext.object.stroke.width * 2;
+  return (
+    <svg
+      className={clsx('object-rect', 'absolute')}
+      width={animation.rect.width + containerContext.transform.width + margin}
+      height={animation.rect.height + containerContext.transform.height + margin}
+      style={{
+        top: `${animation.rect.y + containerContext.transform.y - (margin / 2)}px`,
+        left: `${animation.rect.x + containerContext.transform.x - (margin / 2)}px`,
+      }}
+    >
+      <g>
+        <rect
+          x={margin / 2}
+          y={margin / 2}
+          width={animation.rect.width + containerContext.transform.width}
+          height={animation.rect.height + containerContext.transform.height}
+          fill={containerContext.object.fill.color}
+          stroke={containerContext.object.stroke.color}
+          strokeWidth={containerContext.object.stroke.width}
+          strokeDasharray={containerContext.object.stroke.dasharray}
+        ></rect>
+      </g>
+    </svg>
+  );
 };
