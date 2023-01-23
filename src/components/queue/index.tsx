3f19a197cda8908c4ea0d9a7522e1106dce35b5e
winetree94 <winetree94@outlook.com>
2023-01-24 01:30:24 +0900

    chores: change resizer directory

--- src/components/queue/index.tsx
+++ src/components/queue/index.tsx
@@ -1,15 +1,13 @@
-import { ObjectResizer } from 'cdk/resizer/Resizer';
 import { QueueObjectContainer } from './Container';
 import { Draggable } from './Draggable';
-import { LegacyQueueObject } from './EditableObject';
 import { ObjectAnimator } from './QueueAnimation';
 import { Rect } from './Rect';
+import { ObjectResizer } from './Resizer';
 import { Text } from './Text';
 
 export const QueueObject = {
   Drag: Draggable,
   Container: QueueObjectContainer,
-  Legacy: LegacyQueueObject,
   Animator: ObjectAnimator,
   Rect: Rect,
   Resizer: ObjectResizer,
