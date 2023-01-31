1912676b0ab8fdc9cec22770f2ada5c7de373d6c
winetree94 <winetree94@outlook.com>
2023-01-31 22:20:30 +0900

    feat: Page Dragging

--- src/app/bottom-panel/BottomPanel.tsx
+++ src/app/bottom-panel/BottomPanel.tsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx';
-import { QueueButton, QueueIconButton } from 'components/button/Button';
-import { FunctionComponent, ReactNode } from 'react';
+import { QueueIconButton } from 'components/button/Button';
+import { FunctionComponent, ReactNode, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { documentState } from 'store/document';
 import { documentSettingsState } from 'store/settings';
 import * as QueueContextMenu from 'components/queue-context-menu/Context';
 import styles from './BottomPanel.module.scss';
+import { QueueToggleGroupItem, QueueToogleGroupRoot } from 'components/toggle-group/ToogleGroup';
 
 export interface BottomPanelProps {
   children?: ReactNode;
@@ -16,6 +17,7 @@ export const BottomPanel: FunctionComponent<BottomPanelProps> = ({
 }) => {
   const [queueDocument, setQueueDocument] = useRecoilState(documentState);
   const [settings, setSettings] = useRecoilState(documentSettingsState);
+  const [dragOverIndex, setDragOverIndex] = useState(-1);
 
   const createPageAndMove = (): void => {
     setQueueDocument({
@@ -111,54 +113,87 @@ export const BottomPanel: FunctionComponent<BottomPanelProps> = ({
     });
   };
 
+  const onDragStart = (
+    e: React.DragEvent<HTMLSpanElement>,
+    index: number,
+  ): void => {
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/plain', `${index}`);
+  };
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>): boolean => {
+    e.preventDefault();
+    return false;
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>): void => {
+    const from = Number(e.dataTransfer.getData('text/plain'));
+    if (!e.currentTarget || !e.currentTarget.classList.contains('page-item')) {
+      return;
+    }
+    const to = Number(e.currentTarget.getAttribute('data-index'));
+    movePage(from, to);
+  };
+
   return (
     <div className={clsx(styles.container)}>
-      <div className={clsx('flex')}>
-        {queueDocument.pages.map((page, index, self) => (
-          <QueueContextMenu.Root key={index}>
-            <QueueContextMenu.Trigger>
-              <QueueButton
-                key={index}
+      <div className={clsx(styles.pageHolder)}>
+        <QueueToogleGroupRoot
+          type="single"
+          value={`${settings.queuePage}`}
+          onValueChange={(value): void => navigatePage(Number(value))}
+        >
+          {queueDocument.pages.map((page, index, self) => (
+            <QueueContextMenu.Root key={index}>
+              <QueueContextMenu.Trigger
                 className={clsx(
-                  styles.pageButton,
-                  settings.queuePage === index && styles.selected,
+                  'page-item',
+                  dragOverIndex === index && styles.dragOver,
                 )}
-                onClick={(): void => navigatePage(index)}>
-                {page.pageName}
-              </QueueButton>
-            </QueueContextMenu.Trigger>
-            <QueueContextMenu.Portal>
-              <QueueContextMenu.Content
-                onInteractOutside={(e): void => console.log(e)}>
-                <QueueContextMenu.Item onClick={(): void => movePage(index, Math.max(index - 1, 0))}>
-                  왼쪽으로 페이지 이동
-                </QueueContextMenu.Item>
-                <QueueContextMenu.Item onClick={(): void => movePage(index, Math.min(index + 1, self.length - 1))}>
-                  오른쪽으로 페이지 이동
-                </QueueContextMenu.Item>
-                <QueueContextMenu.Separator />
-                <QueueContextMenu.Item onClick={(): void => createPage(Math.max(index, 0))}>
-                  왼쪽에 페이지 추가
-                </QueueContextMenu.Item>
-                <QueueContextMenu.Item onClick={(): void => createPage(Math.min(index + 1, self.length - 1))}>
-                  오른쪽에 페이지 추가
-                </QueueContextMenu.Item>
-                <QueueContextMenu.Separator />
-                <QueueContextMenu.Item
-                  className={styles.Remove}
-                  onClick={(): void => removePage(index)}>
-                  페이지 삭제
-                </QueueContextMenu.Item>
-              </QueueContextMenu.Content>
-            </QueueContextMenu.Portal>
-          </QueueContextMenu.Root>
-        ))}
+                draggable="true"
+                data-index={index}
+                onDragStart={(e): void => onDragStart(e, index)}
+                onDragEnter={(): void => setDragOverIndex(index)}
+                onDragEnd={(): void => setDragOverIndex(-1)}
+                onDragOver={onDragOver}
+                onDrop={onDrop}>
+                <QueueToggleGroupItem
+                  value={`${index}`}>
+                  {page.pageName}
+                </QueueToggleGroupItem>
+              </QueueContextMenu.Trigger>
+              <QueueContextMenu.Portal>
+                <QueueContextMenu.Content>
+                  <QueueContextMenu.Item onClick={(): void => movePage(index, Math.max(index - 1, 0))}>
+                    왼쪽으로 페이지 이동
+                  </QueueContextMenu.Item>
+                  <QueueContextMenu.Item onClick={(): void => movePage(index, Math.min(index + 1, self.length - 1))}>
+                    오른쪽으로 페이지 이동
+                  </QueueContextMenu.Item>
+                  <QueueContextMenu.Separator />
+                  <QueueContextMenu.Item onClick={(): void => createPage(Math.max(index, 0))}>
+                    왼쪽에 페이지 추가
+                  </QueueContextMenu.Item>
+                  <QueueContextMenu.Item onClick={(): void => createPage(Math.min(index + 1, self.length - 1))}>
+                    오른쪽에 페이지 추가
+                  </QueueContextMenu.Item>
+                  <QueueContextMenu.Separator />
+                  <QueueContextMenu.Item
+                    className={styles.Remove}
+                    onClick={(): void => removePage(index)}>
+                    페이지 삭제
+                  </QueueContextMenu.Item>
+                </QueueContextMenu.Content>
+              </QueueContextMenu.Portal>
+            </QueueContextMenu.Root>
+          ))}
+        </QueueToogleGroupRoot>
       </div>
       <div>
         <QueueIconButton
           className={clsx(styles.pageButton, styles.button)}
           onClick={(): void => createPageAndMove()}>
-          Add
+          <i className='ri-add-fill'></i>
         </QueueIconButton>
       </div>
     </div>
