7cacc319f105a2dcccecd490c0dad654ee9f87e4
winetree94 <winetree94@outlook.com>
2023-02-07 22:46:56 +0900

    feat: New Document - basic markup

--- src/app/new-document-dialog/NewDocumentDialog.tsx
+++ src/app/new-document-dialog/NewDocumentDialog.tsx
@@ -1,8 +1,12 @@
 import { QueueDialog } from 'components/dialog/Dialog';
 import * as Dialog from '@radix-ui/react-dialog';
 import React from 'react';
-import { QueueInput } from 'components/input/Input';
 import { QueueButton } from 'components/button/Button';
+import { QueueSelect } from 'components/select/Select';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import { QueueToggleGroup } from 'components/toggle-group/ToggleGroup';
+import clsx from 'clsx';
+import styles from './NewDocumentDialog.module.scss';
 
 export interface NewDocumentDialogProps extends Omit<Dialog.DialogProps, 'children'> {
   onSubmit?: () => void;
@@ -12,6 +16,10 @@ export const NewDocumentDialog: React.FC<NewDocumentDialogProps> = ({
   onSubmit,
   ...props
 }) => {
+
+  const [documentRatio, setDocumentRatio] = React.useState<string>('16:9');
+  const [documentTemplate, setDocumentTemplate] = React.useState<string>('empty');
+
   return (
     <QueueDialog.Root {...props}>
       <QueueDialog.Portal>
@@ -20,10 +28,63 @@ export const NewDocumentDialog: React.FC<NewDocumentDialogProps> = ({
           <QueueDialog.Title>
             새 문서 설정
           </QueueDialog.Title>
-          <QueueDialog.Description>
+          <div className={styles.Container}>
+            <div className={styles.RatioContainer}>
+              <h6>
+                문서 크기 비율
+              </h6>
+              <QueueSelect.Root
+                value={documentRatio}
+                onValueChange={(value): void => setDocumentRatio(value)}
+              >
+                <QueueSelect.Trigger className={styles.RatioSelector} aria-label="Food">
+                  <QueueSelect.Value />
+                  <QueueSelect.Icon className="SelectIcon">
+                    <ChevronDownIcon />
+                  </QueueSelect.Icon>
+                </QueueSelect.Trigger>
+                <QueueSelect.Portal>
+                  <QueueSelect.Content className="SelectContent">
+                    <QueueSelect.Viewport className="SelectViewport">
+                      <QueueSelect.Group>
+                        <QueueSelect.Item value="16:9">와이드 (16:9)</QueueSelect.Item>
+                        <QueueSelect.Item value="16:10">와이드 (16:10)</QueueSelect.Item>
+                        <QueueSelect.Item value="4:3">35mm (4:3)</QueueSelect.Item>
+                      </QueueSelect.Group>
+                    </QueueSelect.Viewport>
+                  </QueueSelect.Content>
+                </QueueSelect.Portal>
+              </QueueSelect.Root>
+            </div>
+            <div className={styles.TemplateContainer}>
+              <QueueToggleGroup.Root
+                type="single"
+                value={documentTemplate}
+                onValueChange={(value): void => setDocumentTemplate(value)}
+                className={clsx(styles.TemplateGroup)}
+              >
+                <QueueToggleGroup.Item
+                  className={styles.TemplateItem}
+                  value="empty">
+                  template
+                </QueueToggleGroup.Item>
+                <QueueToggleGroup.Item
+                  value="2323"
+                  className={styles.TemplateItem}>
+                  template
+                </QueueToggleGroup.Item>
+                <QueueToggleGroup.Item
+                  value="123213"
+                  className={styles.TemplateItem}>
+                  template
+                </QueueToggleGroup.Item>
+              </QueueToggleGroup.Root>
+            </div>
+          </div>
+          {/* <QueueDialog.Description>
             <QueueInput
               required />
-          </QueueDialog.Description>
+          </QueueDialog.Description> */}
           <QueueDialog.Footer>
             <QueueButton
               type="button"
