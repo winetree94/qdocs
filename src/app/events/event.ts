2c1d37bb1d657ed54e9105f3b79370ded413fff7
winetree94 <winetree94@outlook.com>
2023-02-22 01:06:31 +0900

    chores: add event example

--- src/app/events/event.ts
+++ src/app/events/event.ts
@@ -1,3 +1,4 @@
 import { createEvent } from 'cdk/hooks/event-dispatcher';
 
 export const fitScreenSizeEvent = createEvent('fitScreenSizeEvent');
+export const someEvent = createEvent<{ data: string }>('otherEvent');
