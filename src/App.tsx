d58eb9023a112fb8c71e4f9b4dd405a5775e8ac0
winetree94 <winetree94@outlook.com>
2023-02-22 00:58:53 +0900

    feat: EventDispatch hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux';
 import 'tailwindcss/tailwind.css';
 import './App.scss';
 import { store } from 'store';
+import { EventDispatcherProvider } from 'cdk/hooks/event-dispatcher';
 
 const App: FunctionComponent = () => {
   return (
     <Provider store={store}>
-      <RootLayout></RootLayout>
+      <EventDispatcherProvider>
+        <RootLayout></RootLayout>
+      </EventDispatcherProvider>
     </Provider>
   );
 };
---
+++ src/cdk/hooks/event-dispatcher/context.tsx
@@ -0,0 +1,51 @@
+import { createContext, useMemo, useRef } from 'react';
+import { EventCallback, EventPayloadAction } from './hooks';
+
+interface EventDispatcherContextType {
+  subscribe: <P = void>(eventName: string, emitter: EventCallback<P>) => void;
+  unsubscribe: <P = void>(eventName: string, emitter: EventCallback<P>) => void;
+  dispatch: <P = void>(payload: EventPayloadAction<P>) => void;
+}
+
+export const EventDispatcherContext = createContext<EventDispatcherContextType>(null);
+
+export type EventDispatcherProviderProps = {
+  children: React.ReactNode;
+};
+
+export const EventDispatcherProvider: React.FC<EventDispatcherProviderProps> = ({ children }) => {
+  const listeners = useRef<Record<string, EventCallback<never>[]>>({});
+
+  const subscribe = <P = void,>(eventName: string, emitter: EventCallback<P>): void => {
+    listeners.current = {
+      ...listeners.current,
+      [eventName]: [...(listeners.current[eventName] || []), emitter],
+    };
+  };
+
+  const unsubscribe = <P = void,>(eventName: string, emitter: EventCallback<P>): void => {
+    listeners.current = {
+      ...listeners.current,
+      [eventName]: (listeners.current[eventName] || []).filter((listener) => listener !== emitter),
+    };
+  };
+
+  const dispatch = <P = void,>(payload: EventPayloadAction<P>): void => {
+    const emitters = listeners.current[payload.type] as EventCallback<P>[];
+    if (!emitters) {
+      return;
+    }
+    emitters.forEach((emitter) => emitter(payload));
+  };
+
+  const memoizedValue = useMemo(
+    () => ({
+      subscribe,
+      unsubscribe,
+      dispatch,
+    }),
+    [],
+  );
+
+  return <EventDispatcherContext.Provider value={memoizedValue}>{children}</EventDispatcherContext.Provider>;
+};
