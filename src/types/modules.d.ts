7e4de3402617ee8565d433f7e9e079f5faedee54
winetree94 <winetree94@outlook.com>
2023-03-13 20:14:43 +0900

    chores: remove unused d.ts

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,25 +1,3 @@
-declare module '*.svg' {
-  import React = require('react');
-
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
-
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.module.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module '*.que' {
   const que: string;
   export default que;
