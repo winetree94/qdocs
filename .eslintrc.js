62fad00cb9f046b04642a9e2382a68dbf77bfde7
duchangkim <duchang.dev@gmail.com>
2023-07-29 15:44:42 +0900

    feat: Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'react-app', 'react-app/jest', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'react-app', 'react-app/jest', 'prettier', 'plugin:storybook/recommended'],
   settings: {
     'import/resolver': {
       node: {
         paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
   },
   plugins: ['prettier'],
   rules: {
@@ -15,22 +15,13 @@ module.exports = {
     quotes: ['error', 'single'],
     indent: 'off',
     'no-trailing-spaces': ['error'],
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'variable',
-        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
-      },
-    ],
+    '@typescript-eslint/naming-convention': ['error', {
+      selector: 'variable',
+      format: ['camelCase', 'UPPER_CASE', 'PascalCase']
+    }],
     '@typescript-eslint/typedef': ['error'],
     '@typescript-eslint/ban-types': ['error'],
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-non-null-assertion': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-  },
+  }
 };
