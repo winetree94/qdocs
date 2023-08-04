da48435982e1bf5871b58a1a21c7f9e86b139328
duchangkim <duchang.dev@gmail.com>
2023-08-04 12:59:32 +0900

    feat: [WIP] Add dropdown component

---
+++ src/components/dropdownV2/Dropdown.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta } from '@storybook/react';
+import { QueueDropdown } from './Dropdown';
+
+// type Story = StoryObj<typeof QueueDropdown>;
+
+const meta: Meta<typeof QueueDropdown> = {
+  title: 'Components/Dropdown',
+  // component: QueueDropdown,
+  parameters: {},
+};
+
+export const Primary ={
+  render: () => {
+    <div>냐냥이</div>;
+  }
+};
+
+export default meta;
