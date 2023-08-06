bc8528aac57ceefc922e938fa061a1ef692230fb
Woochul Hyun <woochul.hyun91@gmail.com>
2023-08-07 01:37:10 +0900

    feat: add QueueButtonGroup to rightPanel

--- src/app/right-panel/RightPanel.tsx
+++ src/app/right-panel/RightPanel.tsx
@@ -1,4 +1,8 @@
 import { QueueControlInputBoxComponent } from 'components/control-input-box/components';
+import QueueButtonGroup from 'components/buttons/button-group/ButtonGroup';
+import { QueueButton } from '../../components/button/Button';
+import { QUEUE_UI_SIZE } from 'styles/ui/Size';
+import { QUEUE_UI_COLOR } from 'styles/ui/Color';
 
 export interface RightPanelProps extends React.HTMLAttributes<HTMLDivElement> {
   hello?: 'world';
@@ -11,13 +15,47 @@ const inputValueChanges = (e: number) => {
 export const RightPanel = ({ hello, ...props }: RightPanelProps) => {
   return (
     <>
-    <div {...props}>
-      우측 패널임
-      <div className="tw-flex">
-      <QueueControlInputBoxComponent.IconInput valueChangeEvent={inputValueChanges} unit='degree'></QueueControlInputBoxComponent.IconInput>
-      <QueueControlInputBoxComponent.Input valueChangeEvent={inputValueChanges} unit='percent'></QueueControlInputBoxComponent.Input>
+      <div {...props}>
+        우측 패널임
+        <div className="tw-flex">
+          <QueueControlInputBoxComponent.IconInput
+            valueChangeEvent={inputValueChanges}
+            unit="degree"></QueueControlInputBoxComponent.IconInput>
+          <QueueControlInputBoxComponent.Input
+            valueChangeEvent={inputValueChanges}
+            unit="percent"></QueueControlInputBoxComponent.Input>
+        </div>
+        <QueueButtonGroup>
+          <QueueButton
+            type="button"
+            size={QUEUE_UI_SIZE.MEDIUM}
+            color={QUEUE_UI_COLOR.RED}
+            onClick={() => console.log(1)}>
+            btn1
+          </QueueButton>
+          <QueueButton
+            type="button"
+            size={QUEUE_UI_SIZE.MEDIUM}
+            color={QUEUE_UI_COLOR.BLUE}
+            onClick={() => console.log(2)}>
+            btn2
+          </QueueButton>
+          <QueueButton
+            type="button"
+            size={QUEUE_UI_SIZE.MEDIUM}
+            color={QUEUE_UI_COLOR.DEFAULT}
+            onClick={() => console.log(3)}>
+            btn3
+          </QueueButton>
+          <QueueButton
+            type="button"
+            size={QUEUE_UI_SIZE.MEDIUM}
+            color={QUEUE_UI_COLOR.RED}
+            onClick={() => console.log(4)}>
+            btn4
+          </QueueButton>
+        </QueueButtonGroup>
       </div>
-    </div>
     </>
   );
 };
