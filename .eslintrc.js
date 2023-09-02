e271cc066a75054244fcf65a9f52c5cf80b9de98
winetree94 <winetree94@gmail.com>
2023-09-02 12:55:52 +0900

    chores: Eslint - add react hooks plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
